2386a192ca148a2b569f0795de8c4d0fe26bd958
Ehab Salah <e.salah82055@gmail.com>
2026-06-27 13:43:38 +0300

    Add Folder Herichaly

---
+++ shopify-ecommerce-ios/scene/Authentication/presentation/Signup/viewModel/SignupViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  SignupViewModel.swift
+//  shopify-ecommerce-ios
+//
+//  Created by Ehab Salah on 27/06/2026.
+//
+
+import Foundation
+
+protocol SignupViewModelProtocol {
+}
+
+
+class SignupViewModel : SignupViewModelProtocol {
+    
+}
---
+++ shopify-ecommerce-ios/scene/Authentication/sharedViews/AuthCustomeButton.swift
@@ -0,0 +1,18 @@
+//
+//  AuthCustomeButton.swift
+//  shopify-ecommerce-ios
+//
+//  Created by Ehab Salah on 27/06/2026.
+//
+
+import SwiftUI
+
+struct AuthCustomeButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthCustomeButton()
+}
