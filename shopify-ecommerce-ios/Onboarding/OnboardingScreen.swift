976d31db04b2853b98aef87cc90a08314085b20d
albaraasayed <161741653+albaraasayed@users.noreply.github.com>
2026-06-27 15:59:23 +0300

    Done

--- shopify-ecommerce-ios/Onboarding/OnboardingScreen.swift
+++ shopify-ecommerce-ios/Onboarding/OnboardingScreen.swift
@@ -7,41 +7,6 @@
 
 import SwiftUI
 
-enum OnboardingPage : Int, CaseIterable {
-    case chooseProducts, makePayment, getYourOrder
-    
-    var image: String {
-        switch self {
-        case .chooseProducts:
-            return "onboarding-image-1"
-        case .makePayment:
-            return "onboarding-image-2"
-        case .getYourOrder:
-            return "onboarding-image-3"
-        }
-    }
-    var title : String {
-        switch self {
-        case .chooseProducts:
-            return "Choose Product"
-        case .makePayment:
-            return "Make Payment"
-        case .getYourOrder:
-            return "Get Your Order"
-        }
-    }
-    
-    var description : String {
-        switch self {
-        case .chooseProducts:
-            return "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit."
-        case .makePayment:
-            return "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit."
-        case .getYourOrder:
-            return "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit."
-        }
-    }
-}
 struct OnboardingScreen: View {
     @State private var currentPage = 0
     @State private var isAnimating = false
