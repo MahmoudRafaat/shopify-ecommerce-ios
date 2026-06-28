d5d55745f5bdafa38c6b939f770b1c7a12eb6b3b
mahmoud raafat <mahmoudrafatt995@gmail.com>
2026-06-28 13:54:33 -0700

    used the CustomTextField instead of using basic TextField

--- shopify-ecommerce-ios/scene/Authentication/presentation/login/view /components/LoginInputView.swift
+++ shopify-ecommerce-ios/scene/Authentication/presentation/login/view /components/LoginInputView.swift
@@ -4,24 +4,37 @@
 //
 //  Created by Mahmoud Raafat Mustafa on 27/06/2026.
 //
-
 import SwiftUI
 
 struct LoginInputView: View {
     @Binding var email: String
     @Binding var password: String
     
+    @State private var emailHasError = false
+    @State private var passwordHasError = false
+    
     var body: some View {
-        VStack(spacing: 16) {
-            TextField("Username or Email", text: $email)
-                .padding()
-                .background(Color(UIColor.systemGray6))
-                .cornerRadius(10)
+        VStack(spacing: 8) {
             
-            TextField("Password", text: $password)
-                .padding()
-                .background(Color(UIColor.systemGray6))
-                .cornerRadius(10)
+            CustomTextField(
+                placeholder: "Username or Email",
+                type: .email,
+                hasError: emailHasError,
+                errorMessage: "Please enter a valid email.",// should be cahnged  for the specfic error message
+                text: $email
+            )
+            //this line should be remvoed when delete the padding in the CustomTextField
+            .padding(.horizontal, -28)
+            
+            CustomTextField(
+                placeholder: "Password",
+                type: .password,
+                hasError: passwordHasError,
+                errorMessage: "Password cannot be empty.", // should be cahnged  for the specfic error message
+                text: $password
+            )
+            //this line should be remvoed when delete the padding in the CustomTextField
+            .padding(.horizontal, -28)
             
             Button(action: {
                 // Forgot password action
@@ -31,6 +44,7 @@ struct LoginInputView: View {
                     .foregroundColor(.pink)
             }
             .frame(maxWidth: .infinity, alignment: .trailing)
+            .padding(.top, 8)
         }
     }
 }
