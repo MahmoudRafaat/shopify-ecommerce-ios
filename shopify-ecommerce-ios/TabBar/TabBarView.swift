410199993e425abbcf13eb8e9a0a8b53c2cc105c
albaraasayed <161741653+albaraasayed@users.noreply.github.com>
2026-06-28 12:54:59 +0300

    Creeate Custom TabBar

---
+++ shopify-ecommerce-ios/TabBar/TabBarView.swift
@@ -0,0 +1,46 @@
+//
+//  TabBar.swift
+//  shopify-ecommerce-ios
+//
+//  Created by albaraa alsayed on 12/01/1448 AH.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    @State private var selectedTab: Tab = .home
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            TabView(selection: $selectedTab) {
+                NavigationStack {
+                    Text("Home Screen")
+                }
+                .tag(Tab.home)
+                NavigationStack {
+                    Text("Wishlist Screen")
+                }
+                .tag(Tab.wishlist)
+                NavigationStack {
+                    Text("Cart Screen")
+                }
+                .tag(Tab.cart)
+                NavigationStack {
+                    Text("Search Screen")
+                }
+                .tag(Tab.search)
+                NavigationStack{
+                    Text("Settings Screen")
+                }
+                .tag(Tab.setting)
+            }
+            .toolbar(.hidden, for: .tabBar)
+            CustomTabBarView(selectedTab: $selectedTab)
+        }
+        .ignoresSafeArea(.keyboard)
+    }
+}
+
+#Preview {
+    TabBarView()
+}
