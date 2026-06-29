125563308b74b8c4e67c20c8777680abed88ab94
Yomnaa-Elzairy <yomn.ayman.elzairy@gmail.com>
2026-06-29 15:53:43 -0400

    added home header view

---
+++ shopify-ecommerce-ios/HomeHeaderView.swift
@@ -0,0 +1,48 @@
+//
+//  HomeHeaderView.swift
+//  shopify-ecommerce-ios
+//
+//  Created by Yomna on 27/06/2026.
+//
+
+import SwiftUI
+
+struct HomeHeaderView: View {
+    var body: some View {
+        VStack{
+            HStack(){
+                Button{
+                    
+                }label: {
+                    Image("lines-icon").foregroundStyle(.black)
+                }
+                Spacer()
+                
+                HStack{
+                    Image("logo")
+                    Text("Stylish").font(.title2).fontWeight(.bold).foregroundStyle(Color(
+                        red: 67 / 255,
+                        green: 146 / 255,
+                        blue: 249 / 255
+                    ))
+                }
+                Spacer()
+                
+                Button{
+                    
+                }label: {
+                    Image("profile")
+                        .resizable().frame(width: 40,height: 40)
+                        .clipShape(Circle())
+                }
+                
+            }.padding(.horizontal,14)
+            
+        
+        }
+    }
+}
+
+#Preview {
+    HomeHeaderView()
+}
---
+++ shopify-ecommerce-ios/SearchField.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  shopify-ecommerce-ios
+//
+//  Created by Yomna on 29/06/2026.
+//
+
+import SwiftUI
+
+struct SearchField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchField()
+}
