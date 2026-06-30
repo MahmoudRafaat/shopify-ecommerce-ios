8c3be930dedb4e2d3b0b3a76b3bfe05f70a41df8
Ehab Salah <e.salah82055@gmail.com>
2026-06-30 19:54:03 +0300

    - Add NetworkConstants class to add help us in networking - Refactor the place of reusable views and modifiers in separated folder

---
+++ shopify-ecommerce-ios/Core/helper/NetworkConstants/NetworkConstants.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkConstants.swift
+//  shopify-ecommerce-ios
+//
+//  Created by Ehab Salah on 30/06/2026.
+//
+
+import Foundation
+
+class NetworkConstants {
+    static let BaseURL = "https://mad46-ios-team4.myshopify.com/admin/api/2026-01"
+    static let AdminToken = "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"
+    static let ApiKey = "aa8d104ab1b323002f6385dd093896ff"
+    
+    static let CreateCustomerEndpoint = "/customers.json"
+}
