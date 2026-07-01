39dcca366d27b15cb084207478e052177a4f6930
mahmoud raafat <mahmoudrafatt995@gmail.com>
2026-07-01 08:41:36 -0700

    refactor(auth): Add AuthAPIRouter for generic Shopify API calls
    - Created AuthAPIRouter enum implementing URLRequestConvertible
    - Refactored ShopifyAuthService to use AuthAPIRouter
    - Removed manual method, parameters, encoder, and headers from service calls
    - Added Encodable extension for dictionary conversion
    - Moved endpoints from NetworkConstants to AuthAPIRouter

--- shopify-ecommerce-ios/scene/Authentication/data/Repo/AuthRepoImp.swift
+++ shopify-ecommerce-ios/scene/Authentication/data/Repo/AuthRepoImp.swift
@@ -1,37 +1,58 @@
 //
-//  AuthRepo.swift
+//  ShopifyAuthService.swift
 //  shopify-ecommerce-ios
 //
-//  Created by Ehab Salah on 29/06/2026.
+//  Created by Ehab Salah on 30/06/2026.
 //
 
 import Foundation
-import FirebaseAuth
-class AuthRepoImp: AuthRepoProtocol {
-    
-    
-    private let authService: AuthServiceProtocol
-    private let shopifyService: ShopifyAuthServiceProtocol
-    init(authService: AuthServiceProtocol = FirebaseAuthService(), shopifyService: ShopifyAuthServiceProtocol = ShopifyAuthService()) {
-        self.authService = authService
-        self.shopifyService = shopifyService
-    }
-    
-    func registerByFireBase(email: String, password: String) async throws -> User? {
-        return try await authService.registerUser(withEmail: email, password: password)
+import Alamofire
+
+protocol ShopifyAuthServiceProtocol {
+    func createCustomer(input: CustomerInput) async throws -> CustomerOutput
+    func searchCustomer(email: String) async throws -> CustomerOutput
+}
+
+class ShopifyAuthService: ShopifyAuthServiceProtocol {
+
+    func createCustomer(input: CustomerInput) async throws -> CustomerOutput {
+        let request = AuthAPIRouter.createCustomer(customer: input)
         
+        let task = AF.request(request)
+            .validate()
+        
+        let dataResponse = await task.serializingData().response
+        if let data = dataResponse.data {
+            let prettyString = JsonHelper.prettyJSON(data)
+            print("Response JSON: \(prettyString)")
+        }
+        
+        try dataResponse.validateAndHandlError()
+        
+        let response = try await task.serializingDecodable(CustomerResponse.self).value
+        return response.customer
     }
     
-    func createCustomerInShopify(customerInput: CustomerInput) async throws -> CustomerOutput {
-        return try await shopifyService.createCustomer(input: customerInput)
+    func searchCustomer(email: String) async throws -> CustomerOutput {
+        let request = AuthAPIRouter.searchCustomer(email: email)
+        
+        let task = AF.request(request)
+            .validate()
+        
+        let dataResponse = await task.serializingData().response
+        if let data = dataResponse.data {
+            let prettyString = JsonHelper.prettyJSON(data)
+            print("Search Response JSON: \(prettyString)")
+        }
+        
+        try dataResponse.validateAndHandlError()
+        
+        let response = try await task.serializingDecodable(CustomerSearchResponse.self).value
+        
+        guard let customer = response.customers.first else {
+            throw LoginError.firebaseUserNotFound
+        }
+        
+        return customer
     }
-    
-    func loginByFireBase(email: String, password: String) async throws -> User? {
-        return try await authService.loginUser(withEmail: email, password: password)
-    }
-    
-    func searchCustomerInShopify(email: String) async throws -> CustomerOutput {
-        return try await shopifyService.searchCustomer(email: email)
-    }
-    
 }
