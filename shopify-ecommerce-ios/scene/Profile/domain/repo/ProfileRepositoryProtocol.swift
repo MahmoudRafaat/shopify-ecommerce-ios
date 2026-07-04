10ec8fd6f57c2050d78a23e2872a2f303df9e772
mahmoud raafat <mahmoudrafatt995@gmail.com>
2026-07-04 06:00:21 -0700

    edit in the protcol and deleted unused entity

--- shopify-ecommerce-ios/scene/Profile/domain/repo/ProfileRepositoryProtocol.swift
+++ shopify-ecommerce-ios/scene/Profile/domain/repo/ProfileRepositoryProtocol.swift
@@ -1,15 +1,16 @@
 //
-//  ShopifyCustomerRepositoryProtocol.swift
+//  ProfileRepository.swift
 //  shopify-ecommerce-ios
 //
-//  Created by Mahmoud Raafat Mustafa on 03/07/2026.
+//  Created by Mahmoud Raafat Mustafa on 02/07/2026.
 //
 
+import Foundation
+import FirebaseAuth
 
-protocol ShopifyCustomerRepositoryProtocol {
-    func getCustomer(id: Int) async throws -> ShopifyCustomer
-    func updateCustomerAddress(id: Int, address: ShopifyAddress) async throws -> ShopifyAddress
-    func getMetafields(customerId: Int, namespace: String) async throws -> [Metafield]
-       func createMetafield(customerId: Int, metafield: Metafield) async throws -> Metafield
-       func updateMetafield(customerId: Int, metafieldId: Int, value: String) async throws -> Metafield
+protocol ProfileRepositoryProtocol {
+    func getProfile(customerId: Int) async throws -> ProfileDisplayModel
+    func updateName(customerId: Int, firstName: String, lastName: String) async throws
+    func updateAddress(customerId: Int, address: ProfileAddress) async throws -> ProfileAddress
+    func updatePaymentDetails(customerId: Int, paymentDetails: PaymentDetails) async throws
 }
--- shopify-ecommerce-ios/scene/Profile/domain/repo/ShopifyCustomer.swift
+++
@@ -1,8 +0,0 @@
-struct ShopifyCustomer {
-    let id: Int
-    let email: String?
-    let firstName: String?
-    let lastName: String?
-    let addresses: [ShopifyAddress]?
-    let defaultAddress: ShopifyAddress?
-}
