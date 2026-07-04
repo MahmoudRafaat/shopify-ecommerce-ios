0c39bdcb0e91988bdafd8cddbe6fe2260bb6274c
mahmoud raafat <mahmoudrafatt995@gmail.com>
2026-07-04 05:59:28 -0700

    edit in the use case to get the profile

--- shopify-ecommerce-ios/scene/Profile/domain/usecase/GetProfileUseCaseProtocol.swift
+++ shopify-ecommerce-ios/scene/Profile/domain/usecase/GetProfileUseCaseProtocol.swift
@@ -1,10 +1,43 @@
+//
+//  ProfileUseCase.swift
+//  shopify-ecommerce-ios
+//
+//  Created by Mahmoud Raafat Mustafa on 04/07/2026.
+//
+
+import Foundation
+
 protocol GetProfileUseCaseProtocol {
     func execute(customerId: Int) async throws -> ProfileDisplayModel
 }
 
 protocol UpdateProfileUseCaseProtocol {
     func updateName(customerId: Int, firstName: String, lastName: String) async throws
-    func updateEmail(customerId: Int, email: String) async throws
-    func updateAddress(customerId: Int, address: ShopifyAddress) async throws -> ShopifyAddress
+    func updateAddress(customerId: Int, address: ProfileAddress) async throws -> ProfileAddress
     func updatePaymentDetails(customerId: Int, paymentDetails: PaymentDetails) async throws
+}
+
+class ProfileUseCase: GetProfileUseCaseProtocol, UpdateProfileUseCaseProtocol {
+    
+    private let repository: ProfileRepositoryProtocol
+    
+    init(repository: ProfileRepositoryProtocol = ProfileRepository()) {
+        self.repository = repository
+    }
+    
+    func execute(customerId: Int) async throws -> ProfileDisplayModel {
+        return try await repository.getProfile(customerId: customerId)
+    }
+    
+    func updateName(customerId: Int, firstName: String, lastName: String) async throws {
+        try await repository.updateName(customerId: customerId, firstName: firstName, lastName: lastName)
+    }
+    
+    func updateAddress(customerId: Int, address: ProfileAddress) async throws -> ProfileAddress {
+        return try await repository.updateAddress(customerId: customerId, address: address)
+    }
+    
+    func updatePaymentDetails(customerId: Int, paymentDetails: PaymentDetails) async throws {
+        try await repository.updatePaymentDetails(customerId: customerId, paymentDetails: paymentDetails)
+    }
 }
