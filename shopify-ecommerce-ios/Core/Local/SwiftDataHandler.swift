c648939b511f75e87a1194bb0cdf43e7a87248cd
Ehab Salah <e.salah82055@gmail.com>
2026-07-07 02:46:09 +0300

    refactor the catch error and doesn't use fetal error

--- shopify-ecommerce-ios/Core/Local/SwiftDataHandler.swift
+++ shopify-ecommerce-ios/Core/Local/SwiftDataHandler.swift
@@ -21,7 +21,9 @@ final class SwiftDataHandler {
         do {
             return try ModelContainer(for: schema, configurations: [modelConfiguration])
         } catch {
-            fatalError("Could not create ModelContainer: \(error)")
+            print("Could not create ModelContainer: \(error)")
+            let fallbackConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: true)
+            return try! ModelContainer(for: schema, configurations: [fallbackConfiguration])
         }
     }()
     
