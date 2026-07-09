86598c6161a4b6e9c57f61fb4b729543398102bc
albaraasayed <161741653+albaraasayed@users.noreply.github.com>
2026-07-09 08:31:38 +0300

    Merge with dev

--- shopify-ecommerce-ios/scene/Authentication/Presentation/Signup/Views/SignupView.swift
+++ shopify-ecommerce-ios/scene/Authentication/Presentation/Signup/Views/SignupView.swift
@@ -89,14 +89,7 @@ struct SignupView: View {
         .showLoading(if: viewmodel.isLoading)
         .showCustomAlert(title: "Error", errorMessage: $viewmodel.errorMessage)
         .background(AppColor.backgroundPrimary.ignoresSafeArea())
-        .fullScreenCover(isPresented: $showHome) {
-            TabBarView()
-        }
-        .onChange(of: viewmodel.isSignupSuccess) { _, newValue in
-            if newValue {
-                showHome = true
-            }
-        }
+
         .sheet(isPresented: $viewmodel.showPhonePopup) {
             GooglePhoneSheet(
                 googlePhone: $viewmodel.googlePhone,
